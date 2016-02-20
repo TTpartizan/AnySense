c046b9eb150a529c3b388d6747d139b41c4763fb
Ling-Jyh Chen <cclljj@gmail.com>
2016-02-20 13:09:26 +0800

    Update config.h

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 // Sense_TH: whether temperature/humidity sensor is used
 // 0: none
 // 1: Grove - Temperature and Humidity Sensor Pro (Digital, D5)
-// 2: Bosch BME280 (I2C) => Sense_Barometer has to be set to 3
-#define Sense_TH        0
+// 2: Bosch BME280 (I2C)
+#define Sense_TH        2
 #define DHTPIN_D 5     // D5
 #define DHTTYPE_D DHT22   // DHT 22  (AM2302)
 
@@ -16,24 +16,23 @@
 // 0: none
 // 1: Grove - Grove - Barometer (High-Accuracy) (I2C)
 // 2: Grove - Barometer Sensor (BMP180) (I2C)
-// 3: Boshi BME280 (I2C)  => Sense_TH has to be set to 2
-#define Sense_Barometer 0
+// 3: Boshi BME280 (I2C)
+#define Sense_Barometer 3
 
 // Sense_PM: whether particulate matter sensor is used
 // 0: none
-// 1: PMS3003 (G3 sensor)
-// 2: PMS5003 (G5 sensor)
-// 3: CP-15-A4 (A4 sensor)
-#define Sense_PM        3
-// 0: using SoftwareSerial (rx:10, tx:11)
-// 1: using UART
-#define Sense_PM_UART   1
+// 1: PMS3003 (G3 sensor) (UART)
+// 2: PMS3003 (G3 sensor) (SoftwareSerial; rx:10, tx:11)
+// 3: CP-15-A4 (A4 sensor) (UART)
+// 4: CP-15-A4 (A4 sensor) (SoftwareSerial; rx:10, tx:11)
+// 5: PMS5003 (G5 sensor) (UART)
+// 6: PMS5003 (G5 sensor) (SoftwareSerial; rx:10, tx:11)
+#define Sense_PM        6
 
 // Output_LCD: whether LCD is used
 // 0: none
 // 1: Grove - LCD RGB Backlight (I2C)
-#define Output_LCD      1
-
+#define Output_LCD      0     
 
 
 
